eb0a3a9cff55de9fe96963994aac5688712a4569
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2024-12-17 02:11:40 +0200

    fix api

--- api/moon.js
+++ api/moon.js
@@ -1,18 +1,14 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-  const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
-  const { city, lat, lon } = req.query; // Приймаємо місто або координати
-  const today = new Date().toISOString().slice(0, 10);
+    const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+    const today = new Date().toISOString().slice(0, 10);
 
-  try {
-    let query = `q=auto:ip`; // За замовчуванням використовуємо IP сервера
-    if (city) query = `q=${city}`;
-    if (lat && lon) query = `q=${lat},${lon}`;
-
-    const response = await axios.get(`https://api.weatherapi.com/v1/astronomy.json?key=${WEATHER_API_KEY}&${query}&dt=${today}`);
-    res.status(200).json(response.data);
-  } catch (error) {
-    res.status(500).send('Error fetching data');
-  }
+    try {
+        const response = await axios.get
+        (`https://api.weatherapi.com/v1/astronomy.json?key=${WEATHER_API_KEY}&q=auto:ip&dt=${today}`);
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).send('Error fetching data');
+    }
 };
