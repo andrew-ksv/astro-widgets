9340541f0b5d9439ea7a4fa988b663a8b26cac3a
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2025-01-15 15:26:58 +0200

    added news feed

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/moon', require('./api/moon'));
 app.use('/api/k-index', require('./api/k-index'));
 app.use('/api/astro-pics', require('./api/astro-pics'));
+app.use('/api/news', require('./api/news'));
 
 //Starting the server
 app.listen(PORT, () => {
