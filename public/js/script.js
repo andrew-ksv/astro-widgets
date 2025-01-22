4c13b28d0faa3e57bc890f3cdc3c51fa21640d5e
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2025-01-22 14:51:19 +0200

    fix bug about generate stars

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,42 @@
 "use strict";
 
-const numStars = 100; //Кількість зірок
 const starsContainer = document.getElementById('stars');
-// Отримати висоту всієї сторінки
-const pageHeight = document.body.scrollHeight;
+let previousPageHeight = 0; //змінна для перевірки зміни висоти
 
-// Створення зірок
-for (let i = 0; i < numStars; i++) {
-    const star = document.createElement('div');
-    star.className = 'star';
-    // Випадкові позиції по всій сторінці
-    star.style.top = `${Math.random() * pageHeight}px`; 
-    star.style.left = `${Math.random() * window.innerWidth}px`;
-     //star.style.animationDelay = `${Math.random() * 5}s`; //Випадкова затримка перед початком анімації
-    starsContainer.appendChild(star);
+function generateStars() {
+    starsContainer.innerHTML = ''; //очищення контейнера
+    const pageHeight = document.body.scrollHeight; //повна висота елемента body
+    const starsPerThousandPixels = 100;
+    const numStars = Math.ceil((pageHeight / 1000) * starsPerThousandPixels);
+
+    for (let i = 0; i < numStars; i++) {
+        const star = document.createElement('div');
+        star.className = 'star';
+        //випадкові позиції по всій сторінці
+        star.style.top = `${Math.random() * pageHeight}px`;
+        star.style.left = `${Math.random() * window.innerWidth}px`;
+        //star.style.animationDelay = `${Math.random() * 5}s`; //Випадкова затримка перед початком анімації
+        starsContainer.appendChild(star); //додаємо div з класом star до контейнера starsContainer
+    }
+    // console.log(document.body.scrollHeight)
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    generateStars(); //генерація зірок після завантаження сторінки
+    const observer = new MutationObserver(() => { //спостерігач за змінами DOM
+        const currentPageHeight = document.body.scrollHeight;
+        if (currentPageHeight !== previousPageHeight) {
+            previousPageHeight = currentPageHeight;
+            generateStars(); //перегенерація зірок
+        }
+    });
+    //спостереження за всім документом
+    observer.observe(document.body, {
+        //слідкувати за додаванням або видаленням дочірніх елементів
+        childList: true,
+        subtree: true,
+    });
+});
 
 // Поточний рік
 document.getElementById('currentYear').textContent = new Date().getFullYear();
