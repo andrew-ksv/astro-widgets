cead4afd5480584595cf164eceb54e02e0af315b
andrew-ksv <127143251+andrew-ksv@users.noreply.github.com>
2026-02-14 18:12:02 +0200

    added cache for latest alert

--- api/latest-alert.js
+++ api/latest-alert.js
@@ -1,10 +1,15 @@
 const axios = require('axios');
 
+const CACHE_DURATION = 60; //секунди
+
 module.exports = async (req, res) => {
   try {
     const response = await axios.get('https://services.swpc.noaa.gov/products/alerts.json');
     const data = response.data;
     const latestMessage = data[0]; //останнє повідомлення
+
+    res.setHeader('Cache-Control', `s-maxage=${CACHE_DURATION}, stale-while-revalidate`);
+    
     res.status(200).json(latestMessage);
   } catch (error) {
     console.error(error);
