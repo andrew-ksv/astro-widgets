8ffc10e2be4d8ee93d16464c7c374e8b45c974cd
andrew-ksv <127143251+andrew-ksv@users.noreply.github.com>
2026-02-14 17:38:11 +0200

    added cache for astro pics

--- api/astro-pics.js
+++ api/astro-pics.js
@@ -2,10 +2,20 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
     const NASA_API_KEY = process.env.NASA_API_KEY;
+    const CACHE_DURATION = 60 * 60; //1 година
 
     try {
-        const response = await axios.get(`https://api.nasa.gov/planetary/apod?count=10&api_key=${NASA_API_KEY}`);
+        res.setHeader(
+            "Cache-Control",
+            `s-maxage=${CACHE_DURATION}, stale-while-revalidate`
+        );
+
+        const response = await axios.get(
+            `https://api.nasa.gov/planetary/apod?count=10&api_key=${NASA_API_KEY}`
+        );
+
         res.status(200).json(response.data);
+
     } catch (error) {
         res.status(500).send('Error fetching data from NASA API');
     }
